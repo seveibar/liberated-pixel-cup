45c2b5439b9d25b4ba5b4ed1a4847fa94c4bbe55
seveibar <seveibar@gmail.com>
2012-08-02 02:09:13 -0400

    Attribution and licensing

--- resources/tile_gen.sh
+++
@@ -1,6 +0,0 @@
-#Gotta have 'convert' AKA Image Magick
-
-#loop through tiles
-for f in tile_*.png
-do convert $f -crop 32x32 "tiles/${f%\.*}_%d.png"
-done
--- resources/tile_grass.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tile_grass" tilewidth="32" tileheight="32">
- <image source="tile_grass.png" trans="ff00ff" width="96" height="192"/>
-</tileset>
